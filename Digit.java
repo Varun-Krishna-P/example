5d02440865b0451245916d33c2083bd3339f1fd7
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2015-03-01 11:18:18 +0530

    Display individual digits.

--- Digit.java
+++ Digit.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Digit {
 	public static void main(String[] args) {
 		int number, i = 0, num, digits;
+		int[] digit;
 		Scanner input = new Scanner(System.in);
 		System.out.print("Please enter the digit: ");
 		try{
@@ -9,11 +10,15 @@ public class Digit {
 			num = number;
 			digits = 0;
 			while(num > 0){
-				//digits = num % 10;		
+				digits = num % 10;
+				digit = new int[]{digits};			
 				num = num / 10;				
 				i++;
+				//System.out.print("The Digits are: "+digits+" ,");
 			}
-			System.out.println("The length of the given: "+number+ " is: "+i);			
+			System.out.println("The length of the given: "+number+ " is: "+i);
+			System.out.println("The Digits are: "+digit+" ,");
+			
 		}
 		catch(Exception exception){
 			exception.printStackTrace();
