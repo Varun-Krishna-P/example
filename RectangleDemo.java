04d5cd3c64aeae482b4b1f0695b1e75d4e7894bd
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2015-03-01 13:48:03 +0530

    Pull the method declartation from the main method and create a new class and declare them.

--- RectangleDemo.java
+++ RectangleDemo.java
@@ -1,46 +1,53 @@
 import java.io.*;
+class Rect{
+	double length,width,area;
+	String colour;
+	public void getLength()
+	{	
+		DataInputStream din = new DataInputStream(System.in);
+		System.out.println("\n Enter the length of the Rectangle:");
+		try{
+			length = Double.parseDouble(din.readLine());
+		}
+		catch(IOException e)
+		{
+			System.out.println(e);
+		}
+	}
+		
+	public void getWidth()
+	{
+		DataInputStream din = new DataInputStream(System.in);
+		System.out.println("\n Enter the width of theRectangle:");
+		try{
+			width = Double.parseDouble(din.readLine());
+		}
+		catch(IOException e)
+		{
+			System.out.println(e);
+		}
+	}
+		
+	public void getColour()
+	{
+		DataInputStream din=new DataInputStream(System.in);
+		System.out.println("\n Enter the colour of the Rectangle:");
+		try{
+			colour = din.readLine();
+		}
+		catch(IOException e)
+		{
+			System.out.println(e);
+		}
+	}
+}
 public class RectangleDemo
 {	
 	public static void main(String[] args) {
-		double length,width,area;
-		String colour;
-		public void get_length()
-		{	
-			DataInputStream din=new DataInputStream(System.in);
-			System.out.println("\n Enter the length of the Rectangle:");
-			try{
-				length=Double.parseDouble(din.readLine());
-			}
-			catch(IOException e)
-			{
-				System.out.println(e);
-			}
-		}
+		Rect rect = new Rect();
+		rect.getLength();
+		rect.getWidth();
+		rect.getColour();
 		
-		public void get_width()
-		{
-			DataInputStream din=new DataInputStream(System.in);
-			System.out.println("\n Enter the width of theRectangle:");
-			try{
-				width = Double.parseDouble(din.readLine());
-			}
-			catch(IOException e)
-			{
-				System.out.println(e);
-			}
-		}
-		
-		public void get_colour()
-		{
-			DataInputStream din=new DataInputStream(System.in);
-			System.out.println("\n Enter the colour of the Rectangle:");
-			try{
-			colour = din.readLine();
-			}
-			catch(IOException e)
-			{
-				System.out.println(e);
-			}
-		}
 	}
 }
